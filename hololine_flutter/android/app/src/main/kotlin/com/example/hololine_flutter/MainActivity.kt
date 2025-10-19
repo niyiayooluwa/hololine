da60926695d327686c20c8a2521fc3217357c8f0
niyiayooluwa <aytholu@gmail.com>
2025-10-19 12:35:20 +0100

    feat: Initial commit
    Sets up the initial project structure for the Hololine application, including the Flutter client and Serverpod server.
    
    Removes the default example/greeting endpoint and related files from the Serverpod server and client. Deletes the native desktop (Linux, macOS, Windows) folders from the Flutter project to focus on mobile and web platforms.
    
    Adds the `serverpod_auth` module to the server and client for handling user authentication. Configures the Flutter app with `go_router` for navigation and adds initial session management setup.

---
+++ hololine_flutter/android/app/src/main/kotlin/com/example/hololine_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hololine_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hololine_server/lib/server.dart
@@ -0,0 +1,38 @@
+import 'package:serverpod/serverpod.dart';
+
+import 'package:hololine_server/src/web/routes/root.dart';
+import 'package:serverpod_auth_server/serverpod_auth_server.dart' as auth;
+
+import 'src/generated/protocol.dart';
+import 'src/generated/endpoints.dart';
+
+// This is the starting point of your Serverpod server. In most cases, you will
+// only need to make additions to this file if you add future calls,  are
+// configuring Relic (Serverpod's web-server), or need custom setup work.
+
+void run(List<String> args) async {
+  // Initialize Serverpod and connect it with your generated code.
+  var pod = Serverpod(
+    args,
+    Protocol(),
+    Endpoints(),
+    authenticationHandler: auth.authenticationHandler,
+  );
+
+  // Setup a default page at the web root.
+  pod.webServer.addRoute(RouteRoot(), '/');
+  pod.webServer.addRoute(RouteRoot(), '/index.html');
+  // Serve all files in the /static directory.
+  pod.webServer.addRoute(
+    RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
+    '/*',
+  );
+
+  auth.AuthConfig.set(auth.AuthConfig(
+    maxAllowedEmailSignInAttempts: 3,
+    validationCodeLength: 6,
+  ));
+
+  // Start the server.
+  await pod.start();
+}
