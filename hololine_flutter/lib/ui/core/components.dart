35737f0b8534b8842b2a4194e000a3b299a3846d
niyiayooluwa <aytholu@gmail.com>
2025-12-09 13:58:57 +0100

    feat: Create barrel file for core UI components
    This commit introduces a new barrel file, `components.dart`, to simplify importing common UI widgets.
    
    This file exports the following UI modules:
    - Buttons
    - TextFields
    - Snackbars
    - Other core widgets
    - Material library

---
+++ hololine_flutter/lib/ui/core/components.dart
@@ -0,0 +1,5 @@
+export 'package:hololine_flutter/ui/core/widgets/buttons/buttons.dart';
+export 'package:hololine_flutter/ui/core/widgets/textfields/hl_textfield.dart';
+export 'package:hololine_flutter/ui/core/widgets/widgets.dart';
+export 'package:hololine_flutter/ui/core/widgets/snackbar/snackbar.dart';
+export 'package:flutter/material.dart';
