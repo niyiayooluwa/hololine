777cf1a6eae64990f0a8e31537d78d759abe02da
niyiayooluwa <aytholu@gmail.com>
2026-06-08 21:58:24 +0100

    refactor(ui): cleanup old core components and introduce shadcn_ui theme

--- hololine_flutter/lib/components/core/components.dart
+++
@@ -1,5 +0,0 @@
-export 'package:hololine_flutter/components/core/widgets/buttons/buttons.dart';
-export 'package:hololine_flutter/components/core/widgets/textfields/hl_textfield.dart';
-export 'package:hololine_flutter/components/core/widgets/widgets.dart';
-export 'package:hololine_flutter/components/core/widgets/snackbar/snackbar.dart';
-export 'package:flutter/material.dart';
